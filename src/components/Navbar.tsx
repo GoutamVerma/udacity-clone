c0bf1811cd5c185ebad9058031a02cef76e83724
Goutam Verma <goutam.verma@bobble.ai>
2024-02-08 19:07:27 +0530

    initial commit|

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="bg-white py-4 fixed w-full top-0 z-10">
+      <div className="container">
+        <img src="./udacity-logo.svg" alt="logo" className="w-100 h-10" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/SignInSignUp.tsx
@@ -0,0 +1,51 @@
+'use client'
+import React, { useState } from 'react';
+import SignInForm from './form/SignInForm';
+import SignUpForm from './form/SignUpForm';
+
+enum Mode {
+  SignIn,
+  SignUp,
+}
+
+const MainComponent: React.FC = () => {
+  const [mode, setMode] = useState(Mode.SignIn);
+
+  const toggleMode = (selectedMode: Mode) => {
+    setMode(selectedMode);
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex justify-around w-full">
+        <div
+          className={`w-full text-sm font-sans p-4 h-full text-center font-semibold cursor-pointer ${
+            mode === Mode.SignUp ? 'bg-white' : ''
+          }`}
+          onClick={() => toggleMode(Mode.SignUp)}
+        >
+          SIGN UP
+        </div>
+        <div
+          className={`w-full text-sm font-sans p-4 h-full text-center font-semibold cursor-pointer ${
+            mode === Mode.SignIn ? 'bg-white' : ''
+          }`}
+          onClick={() => toggleMode(Mode.SignIn)}
+        >
+          SIGN IN
+        </div>
+      </div>
+
+      <div className="w-full">
+        <div style={{ display: mode === Mode.SignIn ? 'block' : 'none' }} className="w-full">
+          <SignInForm />
+        </div>
+        <div style={{ display: mode === Mode.SignUp ? 'block' : 'none' }} className="w-full">
+          <SignUpForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainComponent;
